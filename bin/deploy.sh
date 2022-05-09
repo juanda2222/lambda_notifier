2afbdb9f7a6209b766b6eb36ae3fdd8772203a84
David R <juandara2222@gmail.com>
2022-05-09 17:16:39 -0500

    stable deploy stack with s3

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,31 +8,23 @@ Options:
  --region <AWS_REGION>       AWS Region to perform deploy. Default: us-west-2 region
 "
 
-profile=${profile:-default}
-region=${region:-us-west-2}
+# profile=${profile:-default}
+region=${region:-us-east-1}
 stack=""
 
-get_profile() {
-  if [ -n "${profile}" ];
-  then
-    echo "--profile ${profile}"
-  else
-    echo ""
-  fi
-}
+# get_airflow_url() {
+#   if [[ $stack == *"test"* ]];
+#   then
+#     echo "--parameter-overrides AirflowUrl=https://airflow.test.instride.com"
+#   elif [[ $stack == *"env"* ]];
+#   then 
+#     echo "--parameter-overrides AirflowUrl=https://airflow.corp.instride.com"
+#   else
+#     echo ""
+#   fi
+# }
 
-get_airflow_url() {
-  if [[ $stack == *"test"* ]];
-  then
-    echo "--parameter-overrides AirflowUrl=https://airflow.test.instride.com"
-  elif [[ $stack == *"env"* ]];
-  then 
-    echo "--parameter-overrides AirflowUrl=https://airflow.corp.instride.com"
-  else
-    echo ""
-  fi
-}
-
+# parse the arguments
 while [ $# -gt 0 ]; do
 
    if [[ $1 == *"--"* ]]; then
@@ -43,31 +35,33 @@ while [ $# -gt 0 ]; do
   shift
 done
 
+# validate mandatory parameters
 if [[ -z "${stack}" ]];
 then
   echo "$__usage";
   exit 1;
 fi
 
-if ! aws s3api wait bucket-exists --bucket $stack --region $region $(get_profile); then
-  echo "creating s3 bucket for deploy"
-  aws s3 mb s3://$stack --region $region $(get_profile)
-fi
+# format parameters into ready to use arguments for aws cli
+get_profile() {
+  if [ -n "${profile}" ];
+  then
+    echo "--profile ${profile}"
+  else
+    echo ""
+  fi
+}
 
-if ! aws s3api wait bucket-exists --bucket $stack-delta-etl-scripts --region $region $(get_profile); then
-  echo "creating s3 bucket for etl scripts"
-  aws s3 mb s3://$stack-delta-etl-scripts --region $region $(get_profile)
-fi
 
-aws s3 cp s3-jars/delta-core_2.12-1.0.0.jar s3://$stack-delta-etl-scripts
-aws s3 cp glue-scripts/ingestion/batch_ingestion_etl.py s3://$stack-delta-etl-scripts/ingestion/batch_ingestion_etl.py
-ingestionDB="${stack//[-]/_}"
+# if ! aws s3api wait bucket-exists --bucket $stack --region $region $(get_profile); then
+#   echo "creating s3 bucket for deploy"
+#   aws s3 mb s3://$stack --region $region $(get_profile)
+# fi
 
 echo "Packaging stack on ${stack}"
-aws cloudformation package --region $region $(get_profile) --template-file glue-resources.yml --s3-bucket $stack --output-template-file out.yml
+aws cloudformation package --region $region $(get_profile) --template-file ./aws_resources.yml --s3-bucket $stack --output-template-file out.yml
 
 echo "Deploying stack ${stack}"
-echo "Deploying DB ${ingestionDB}"
-aws cloudformation deploy --region $region $(get_profile) $(get_airflow_url) --template-file out.yml --stack-name $stack --capabilities "CAPABILITY_IAM" --parameter-overrides IngestionDB=$ingestionDB
+aws cloudformation deploy --region $region $(get_profile) --template-file out.yml --stack-name $stack
 
 echo "Deploy Completed"
---
+++ bin/updateFunctions.sh
@@ -0,0 +1,3 @@
+aws lambda update-function-code 
+    --function-name  my-function 
+    --zip-file fileb://my-function.zip
